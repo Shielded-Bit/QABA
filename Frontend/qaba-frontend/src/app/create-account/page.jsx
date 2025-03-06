4fdea0bb9506beddcda9e866a64a1246ff10e2ed
Declanny <chisomhenryg@gmail.com>
2025-03-06 12:39:19 +0100

    signin page

--- Frontend/qaba-frontend/src/app/create-account/page.jsx
+++ Frontend/qaba-frontend/src/app/create-account/page.jsx
@@ -177,7 +177,7 @@ const Register = () => {
               <div className="mb-5 pt-1">
                 <RoleSelection formData={formData} handleRoleChange={handleRoleChange} />
               </div>
-              <button type="submit" className="mt-10 w-full bg-gradient-to-r from-[#014d98] to-[#3ab7b1] px-4 py-2 text-white transition-all duration-300 hover:from-[#3ab7b1] hover:to-[#014d98] disabled:opacity-50 disabled:cursor-not-allowed" disabled={loading}>{loading ? "Registering..." : "Sign Up"}</button>
+              <button type="submit" className="mt-10 w-full bg-gradient-to-r from-[#014d98] to-[#3ab7b1] px-4 py-2 rounded-md text-white transition-all duration-300 hover:from-[#3ab7b1] hover:to-[#014d98] disabled:opacity-50 disabled:cursor-not-allowed" disabled={loading}>{loading ? "Registering..." : "Sign Up"}</button>
             </form>
             <SignInWithGoogle bgpict={bgpict} />
           </div>
