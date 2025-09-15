bbc11b8afd031929b1ca848b75052a437ca84913
MichaelDecent <michaeldecent2@gmail.com>
2025-09-15 20:08:49 +0100

    Update email backend configuration for development and production settings

--- Backend/qaba-api/core/settings/production.py
+++ Backend/qaba-api/core/settings/production.py
@@ -43,8 +43,8 @@ SECURE_HSTS_PRELOAD = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = getenv("EMAIL_HOST")
-EMAIL_PORT = int(getenv("EMAIL_PORT"))
-EMAIL_USE_TLS = getenv("EMAIL_USE_TLS")
+EMAIL_PORT = int(getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
