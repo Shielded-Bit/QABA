ef6234f116e229cf7fc81fe51257ef4458b7edf8
MichaelDecent <michaeldecent2@gmail.com>
2025-10-08 17:31:26 +0100

    added mfa

--- Backend/qaba-api/core/urls.py
+++ Backend/qaba-api/core/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import include, path
+from two_factor import urls as two_factor_urls
+from two_factor.admin import AdminSiteOTPRequired
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+admin.site.__class__ = AdminSiteOTPRequired
+
+two_factor_patterns, two_factor_app_name = two_factor_urls.urlpatterns
+
 urlpatterns = [
+    path("", include((two_factor_patterns, two_factor_app_name), namespace="two_factor")),
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.users.urls')),
     path('api/v1/', include('apps.properties.urls')),
