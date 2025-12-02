e610c4cb11712ae3d24469ee08f872e012f1e43b
MichaelDecent <michaeldecent2@gmail.com>
2025-12-02 21:29:22 +0100

    Add transaction model dependency in property caution fee migration

--- Backend/qaba-api/apps/properties/migrations/0022_property_caution_fee_property_service_charge.py
+++ Backend/qaba-api/apps/properties/migrations/0022_property_caution_fee_property_service_charge.py
@@ -54,6 +54,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ("properties", "0021_alter_property_agent_commission_and_more"),
         ("users", "0012_remove_user_email_verification_token_otp"),  # OTP model dependency
+        ("transactions", "0001_initial"),  # Transaction model dependency
     ]
 
     operations = [
